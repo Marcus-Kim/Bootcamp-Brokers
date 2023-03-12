abe657eb6eac930b6c8f60075b4adbc7a10c8039
Marcus Kim <kim.marcus97@gmail.com>
2023-03-11 17:16:25 -0700

    added chat_gpt and messages tables with relationships

--- app/models/portfolio.py
+++ app/models/portfolio.py
@@ -12,6 +12,6 @@ class Portfolio(db.Model):
     profit_loss = db.Column(db.Float(asdecimal=True, decimal_return_scale=2), nullable=False)
     cash_balance = db.Column(db.Float(asdecimal=True, decimal_return_scale=2), nullable=False)
 
-    user = db.relationship("User", uselist=False, back_populate="portfolio")
+    user = db.relationship("User", uselist=False, back_populates="portfolio")
     portfolio_shares = db.relationship("PortfolioShare", back_populates="portfolio")
     portfolio_values = db.relationship("PortfolioValue", back_populates="portfolio")
