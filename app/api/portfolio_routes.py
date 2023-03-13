34d89872d3c6bb988a8615d66f5bd052afc545f5
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-13 13:06:32 -0700

    Create get portfolio by id route in portfolio_routes on backend

--- app/api/portfolio_routes.py
+++ app/api/portfolio_routes.py
@@ -4,4 +4,10 @@ from app.models import Portfolio, db
 
 portfolio_routes = Blueprint('portfolios', __name__)
 
-
+# Get portfolio by id
+@portfolio_routes.route('/<int:id>')
+# @ login_required
+def portfolio_by_id(id):
+    """Route for getting a portfolio by id"""
+    portfolio = Portfolio.query.get(id)
+    return portfolio.to_dict()
