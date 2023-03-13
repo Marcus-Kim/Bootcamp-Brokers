ee19db51847e8a7f1aa6528263034a445f50e32f
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-13 13:27:48 -0700

    Connect portfolio_routes blueprint to flask app

--- app/api/portfolio_routes.py
+++ app/api/portfolio_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_login import login_required
 from app.models import Portfolio, db
 
-portfolio_routes = Blueprint('portfolios', __name__)
+portfolio_routes = Blueprint('portfolio', __name__)
 
 # Get portfolio by id
 @portfolio_routes.route('/<int:id>')
