a243e3d2afe4a7158db6444d4ba1de4b461359a3
Marcus Kim <kim.marcus97@gmail.com>
2023-03-13 14:09:44 -0700

    transaction routes done

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -1,22 +1,40 @@
-from flask import Blueprint
-from flask_login import login_required
+from flask import Blueprint, request
+from flask_login import login_required, current_user
 from app.models import Transaction
+from ..models.db import db
 
 
 transaction_routes = Blueprint('transactions', __name__)
 
-#TODO Get all transactions
+# Get all transactions of logged in user
 @transaction_routes.route('/')
 @login_required
 def get_all_transactions():
-    transactions = Transaction.query.all()
+    transactions = Transaction.query.filter_by(user_id=current_user.id).all()
 
+    result = [ transaction.to_dict() for transaction in transactions]
+    return result
 
-
-
-
-#TODO Transactions by stock ticker
+# Get all transaction of logged in user by ticker
 @transaction_routes.route('/<string:id>')
 @login_required
-def get_transactions_by_ticker():
-    transactions = Transaction.query.filter_by()
+def get_transactions_by_ticker(id):
+    ticker = id.upper()
+    transactions = Transaction.query.filter_by(user_id=current_user.id).filter_by(ticker_id=ticker)
+    result = [ transaction.to_dict() for transaction in transactions ]
+    return result
+
+# Create a transaction (need ticker_id and shares)
+@transaction_routes.route('/', methods=['POST'])
+@login_required
+def create_transaction():
+    req_body = request.get_json()
+
+    user_id = current_user.id
+    ticker_id = req_body['ticker_id']
+    shares = req_body['shares']
+
+    new_transaction = Transaction(user_id=user_id, ticker_id=ticker_id, shares=shares)
+    db.session.add(new_transaction)
+    db.session.commit()
+    return new_transaction.to_dict()
---
+++ react-app/src/components/Watchlists/WatchlistDetails.js
@@ -0,0 +1,22 @@
+import './watchlistDetails.css'
+
+
+
+export default function WatchlistDetails() {
+
+
+
+  return (
+    <div className="watchlist-details-container">
+      <div className='watchlist-details-content-container'>
+        <div className='watchlist-details-content'>
+          <h2>List Name</h2>
+        </div>
+        <div className='watchlist-details-lists'> {/* LIST COMPONENT */}
+          <div>LIST COMPONENT</div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
