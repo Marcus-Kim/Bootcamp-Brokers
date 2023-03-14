00b11a007e21559e402a57f2a5e7d96eef1e2e52
Marcus Kim <kim.marcus97@gmail.com>
2023-03-14 10:02:52 -0700

    fixed navigating to watchlists from watchlistdetails

--- react-app/src/components/Watchlists/Watchlists.js
+++ react-app/src/components/Watchlists/Watchlists.js
@@ -5,10 +5,8 @@ import { useNavigate } from 'react-router-dom'
 function Watchlists({ watchlists }) {
   const navigate = useNavigate();
 
-  if (!watchlists.length) {
-    return null
-  }
-  
+  const watchlistArray = Object.values(watchlists)
+
   return (
     <div className='watchlists-container'>
       <div className='watchlists-header'>
@@ -16,7 +14,7 @@ function Watchlists({ watchlists }) {
         <button className='add-watchlist-button'>+</button>
       </div>
       <div className='watchlists-list'>
-        {watchlists.map(watchlist => {
+        {watchlistArray.map(watchlist => {
           return(
             <div className='watchlist-item'>
               <div className='watchlist-item-name' onClick={e => navigate(`/watchlists/${watchlist.id}`)}>{watchlist.list_name}</div>
