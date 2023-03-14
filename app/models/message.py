e45d5638a5349c660446fd81d7dc87454cf55f86
Marcus Kim <kim.marcus97@gmail.com>
2023-03-14 09:01:01 -0700

    another fix for render

--- app/models/message.py
+++ app/models/message.py
@@ -9,18 +9,15 @@ class Message(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'), nullable=False)
-    chat_gpt_instance = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('chat_gpt.id'), ondelete='CASCADE'))
     message = db.Column(db.Text())
     created_at = db.Column(db.Date, default=datetime.now(tz=None), nullable=False)
 
     user = db.relationship("User", back_populates='messages')
-    chat_gpt_instance = db.relationship("Chat_GPT", back_populates='messages')
 
     def to_dict(self):
         return {
             'id': self.id,
             'user_id': self.user_id,
-            'chat_gpt_instance': self.chat_gpt_instance,
             'message': self.message,
             'created_at': self.created_at
         }
