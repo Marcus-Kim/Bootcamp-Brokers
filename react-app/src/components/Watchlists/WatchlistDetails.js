55363e48cbdfba70c5a425a6643bda6f64326e8d
Marcus Kim <kim.marcus97@gmail.com>
2023-03-13 18:30:47 -0700

    working on watchlist details

--- react-app/src/components/Watchlists/WatchlistDetails.js
+++ react-app/src/components/Watchlists/WatchlistDetails.js
@@ -4,18 +4,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { thunkGetAllWatchlistsUserId } from '../../store/watchlist'
 import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export default function WatchlistDetails() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const user = useSelector(state => state.session.user.id)
   const watchlist = useSelector(state => Object.values(state.watchlist))
   const { watchlistId } = useParams()
 
-  console.log(watchlistId)
+  console.log(watchlist)
   useEffect(() => {
-    dispatch(thunkGetAllWatchlistsUserId(user))
+    dispatch(thunkGetAllWatchlistsUserId(watchlistId))
   }, [dispatch])
 
+  if (!watchlist.length) {
+    return null
+  }
 
   return (
     <div className="watchlist-details-container">
@@ -23,17 +28,17 @@ export default function WatchlistDetails() {
         <div className='watchlist-details-content'>
           <div className='watchlist-details-content-header'>
             <div className='watchlist-details-name-buttons'>
-              <div className='watchlist-details-name'>List Name</div>
+              <div className='watchlist-details-name'>{watchlist[0].list_name}</div>
               <div className='watchlist-details-buttons-conatiner'>
                 <button className='watchlist-details-filters-button'>Filters</button>
                 <button className='watchlist-details-more-button'>Three Dots</button>
               </div>
             </div>
-            <div className="watchlist-details-item-count"># Items</div>
+            <div className="watchlist-details-item-count">{watchlist[0].stocks.length} Items</div>
           </div>
-          <table>
-            <thead>
-              <tr>
+          <table className='watchlist-details-table'>
+            <thead className='watchlist-details-table-header'>
+              <tr className='watchlist-details-table-header'>
                 <th className='watchlist-details-list-header-name'>Name</th>
                 <th className='watchlist-details-list-header-symbol'>Symbol</th>
                 <th className='watchlist-details-list-header'>Price</th>
@@ -41,16 +46,20 @@ export default function WatchlistDetails() {
                 <th className='watchlist-details-list-header-market-cap'>Market Cap</th>
               </tr>
             </thead>
-            <tbody>
-              {watchlist.map(watchlist => {
+            <tbody className='watchlist-details-list-body'>
+              {watchlist[0].stocks.map(stock => {
               return (
-              <tr>
-                <td className='watchlist-details-list-header-name'>{watchlist.name}</td>
-                <td className='watchlist-details-list-header-symbol'>Hello</td>
-                <td className='watchlist-details-list-header'>Hello</td>
-                <td className='watchlist-details-list-header'>Hello</td>
-                <td className='watchlist-details-list-header-market-cap'>Hello</td>
-              </tr>
+                <tr className='watchlist-details-list-row' onClick={e => navigate(`/stocks/${stock.ticker}`)}>
+                  <td className='watchlist-details-list-header-name' id='watchlist-details-list-header-name'>{stock.company_name}</td>
+                  <td className='watchlist-details-list-header-symbol'>{stock.ticker}</td>
+                  <td className='watchlist-details-list-header'>{stock.current_price}$</td>
+                  <td className='watchlist-details-list-header'>{stock.daily_change * 100}%</td>
+                  <div id='hello'>
+                    <td className='watchlist-details-list-header-market-cap'>Market Cap</td>
+                    <div className='watchlist-detail-stock-delete-button'>x</div>
+                  </div>
+                </tr>
+
               )
             })}
             </tbody>
