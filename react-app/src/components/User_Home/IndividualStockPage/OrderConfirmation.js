49a9adab0685e3e357967ed47f650cc7abd6670c
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-16 11:16:38 -0700

    Add conditional transaction details OrderConfirmation

--- react-app/src/components/User_Home/IndividualStockPage/OrderConfirmation.js
+++ react-app/src/components/User_Home/IndividualStockPage/OrderConfirmation.js
@@ -1,9 +1,42 @@
 import './OrderConfirmation.css'
 
-export default function OrderConfirmation() {
+export default function OrderConfirmation({ ticker, buySelected, shares }) {
   return (
     <>
-      <h1>Order Confirmation</h1>
+      <div className="purchase-container">
+        <div className="confirmation-title">
+          { ticker + ( buySelected ? ' Purchased!': ' Sold!')}
+        </div>
+        <div className="transaction-details">
+          { buySelected ? 
+            (
+              // Transaction details for a buy order
+              <> 
+                <div className="dollar-amount"></div>
+                <div className="shares">
+                  <div>Shares Purchased</div>
+                  <div>{shares}.0</div>
+                </div>
+                <div className="share-price"></div>
+                <div className="total"></div>
+                <div className="completion-message"></div>
+              </>
+            ) 
+            : (
+              // Transaction details for a sell order
+              <>
+                <div className="shares">
+                  <div>Shares Sold</div>
+                  <div>{shares}.0</div>
+                </div>
+                <div className="share-price"></div>
+                <div className="total"></div>
+                <div ></div>
+              </>
+            )
+          }
+        </div>
+      </div>
     </>
   )
 }
