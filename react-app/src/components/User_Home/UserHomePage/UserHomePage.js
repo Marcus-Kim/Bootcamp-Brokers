aa8b048bd8132e75c3b714679bc60ce6cf120fc9
jason-greenberg <jasongreenberg07@gmail.com>
2023-03-18 13:52:25 -0700

    Make UserHomePage chart render dynamically

--- react-app/src/components/User_Home/UserHomePage/UserHomePage.js
+++ react-app/src/components/User_Home/UserHomePage/UserHomePage.js
@@ -29,7 +29,7 @@ export default function UserHomePage() {
   
   useEffect(() => {
     displayDailyView()
-  },[])
+  },[historicalValues.length])
 
   const verticalLinePlugin = {
     id: "verticalLine",
